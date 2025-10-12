cc463ff9e3bf6a87f802e35d0a89ba6a5cad2976
cherishwish <cherishwish12@gmail.com>
2025-10-12 18:55:50 +0900

    초기 세팅

---
+++ src/main/java/com/be/ebooki/EbookiApplication.java
@@ -0,0 +1,13 @@
+package com.be.ebooki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookiApplication.class, args);
+	}
+
+}
