06678b682974f5af2a492cb8e4e6a2da8884d182
JinSeon Kim <rlawlstjs5573@gmail.com>
2026-01-22 14:47:18 +0900

    ✨Feat: 요금제 조회 메서드 생성 #6

--- src/main/java/com/be/ebooki/repository/UserPlanRepository.java
+++ src/main/java/com/be/ebooki/repository/UserPlanRepository.java
@@ -3,7 +3,9 @@ package com.be.ebooki.repository;
 import com.be.ebooki.domain.User;
 import com.be.ebooki.domain.UserPlan;
 import com.be.ebooki.domain.UserPlanStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,5 +17,6 @@ public interface UserPlanRepository extends JpaRepository<UserPlan, Long> {
     boolean existsByUserAndStatus(User user, UserPlanStatus status);
 
     // 현재 사용 중 요금제 조회
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<UserPlan> findByUserAndStatus(User user, UserPlanStatus status);
 }
